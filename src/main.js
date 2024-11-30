b551b3a936fbcc9369a1a3faaa7b06e9ed607ef8
Francis <vlfmap@gmail.com>
2024-11-30 18:23:12 +0100

    GEM singleton

--- src/main.js
+++ src/main.js
@@ -4,3 +4,12 @@
  * processed in other classes and methods.
  */
 import EngineConfig from '../config/engine.config.json' with {type: 'json'};
+
+export default class GEM {
+    __instance; /**@typedef {instance of GEM} __instance*/
+
+    get getInstance() {
+        if (!this.__instance) this.__instance = new GEM();
+        return this.__instance
+    }
+}
